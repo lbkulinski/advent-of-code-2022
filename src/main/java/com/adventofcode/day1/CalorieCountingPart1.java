6c8202bc5dd20ea4203eeaa5cfcecc840212d2dc
Logan <rashes_lineage02@icloud.com>
2022-12-01 08:58:36 -0600

    Day 1 updates

--- src/main/java/com/adventofcode/day1/CalorieCountingPart1.java
+++ src/main/java/com/adventofcode/day1/CalorieCountingPart1.java
@@ -1,7 +1,7 @@
 package com.adventofcode.day1;
 
 import java.nio.file.Path;
-import java.util.List;
+import java.io.BufferedReader;
 import java.nio.file.Files;
 import java.io.IOException;
 
@@ -9,35 +9,37 @@ public final class CalorieCountingPart1 {
     public static void main(String[] args) {
         Path path = Path.of("src/main/resources/day1/input.txt");
 
-        List<String> lines;
+        int currentSum = 0;
 
-        try {
-            lines = Files.readAllLines(path);
+        int maxSum = 0;
+
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String line = reader.readLine();
+
+            while (line != null) {
+                if (line.isEmpty()) {
+                    if (currentSum > maxSum) {
+                        maxSum = currentSum;
+                    } //end if
+
+                    currentSum = 0;
+
+                    line = reader.readLine();
+
+                    continue;
+                } //end if
+
+                int calorieAmount = Integer.parseInt(line);
+
+                currentSum += calorieAmount;
+
+                line = reader.readLine();
+            } //end while
         } catch (IOException e) {
             e.printStackTrace();
 
             return;
         } //end try catch
-
-        int currentSum = 0;
-
-        int maxSum = 0;
-
-        for (String line : lines) {
-            if (line.isEmpty()) {
-                if (currentSum > maxSum) {
-                    maxSum = currentSum;
-                } //end if
-
-                currentSum = 0;
-
-                continue;
-            } //end if
-
-            int calorieAmount = Integer.parseInt(line);
-
-            currentSum += calorieAmount;
-        } //end for
 
         if (currentSum > maxSum) {
             maxSum = currentSum;
--- src/main/java/com/adventofcode/day1/CalorieCountingPart2.java
+++ src/main/java/com/adventofcode/day1/CalorieCountingPart2.java
@@ -1,50 +1,75 @@
 package com.adventofcode.day1;
 
 import java.nio.file.Path;
-import java.util.List;
+import java.io.BufferedReader;
 import java.nio.file.Files;
 import java.io.IOException;
-import java.util.TreeSet;
 
 public final class CalorieCountingPart2 {
     public static void main(String[] args) {
         Path path = Path.of("src/main/resources/day1/input.txt");
 
-        List<String> lines;
+        int currentSum = 0;
 
-        try {
-            lines = Files.readAllLines(path);
+        int firstMaxSum = 0;
+
+        int secondMaxSum = 0;
+
+        int thirdMaxSum = 0;
+
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String line = reader.readLine();
+
+            while (line != null) {
+                if (line.isEmpty()) {
+                    if (currentSum > firstMaxSum) {
+                        thirdMaxSum = secondMaxSum;
+
+                        secondMaxSum = firstMaxSum;
+
+                        firstMaxSum = currentSum;
+                    } else if (currentSum > secondMaxSum) {
+                        thirdMaxSum = secondMaxSum;
+
+                        secondMaxSum = currentSum;
+                    } else if (currentSum > thirdMaxSum) {
+                        thirdMaxSum = currentSum;
+                    } //end if
+
+                    currentSum = 0;
+
+                    line = reader.readLine();
+
+                    continue;
+                } //end if
+
+                int calorieAmount = Integer.parseInt(line);
+
+                currentSum += calorieAmount;
+
+                line = reader.readLine();
+            } //end while
         } catch (IOException e) {
             e.printStackTrace();
 
             return;
         } //end try catch
 
-        int currentSum = 0;
+        if (currentSum > firstMaxSum) {
+            thirdMaxSum = secondMaxSum;
 
-        TreeSet<Integer> calorieSums = new TreeSet<>();
+            secondMaxSum = firstMaxSum;
 
-        for (String line : lines) {
-            if (line.isEmpty()) {
-                calorieSums.add(currentSum);
+            firstMaxSum = currentSum;
+        } else if (currentSum > secondMaxSum) {
+            thirdMaxSum = secondMaxSum;
 
-                currentSum = 0;
+            secondMaxSum = currentSum;
+        } else if (currentSum > thirdMaxSum) {
+            thirdMaxSum = currentSum;
+        } //end if
 
-                continue;
-            } //end if
-
-            int calorieAmount = Integer.parseInt(line);
-
-            currentSum += calorieAmount;
-        } //end for
-
-        calorieSums.add(currentSum);
-
-        int topThreeSum = calorieSums.descendingSet()
-                                     .stream()
-                                     .limit(3)
-                                     .reduce(Integer::sum)
-                                     .orElseThrow();
+        int topThreeSum = firstMaxSum + secondMaxSum + thirdMaxSum;
 
         System.out.printf("Top Three Sum: %d%n", topThreeSum);
     } //main
