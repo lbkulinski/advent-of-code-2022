12a59dc58c244b4bb543ded6108c3ca77c723f06
Logan <rashes_lineage02@icloud.com>
2022-12-04 18:04:12 -0600

    Day 4

---
+++ src/main/java/com/adventofcode/day4/CampCleanupPart1.java
@@ -0,0 +1,86 @@
+package com.adventofcode.day4;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class CampCleanupPart1 {
+    private record Range(int start, int end) {
+        public Range {
+            if (start > end) {
+                throw new IllegalArgumentException();
+            } //end if
+        } //Range
+
+        public boolean overlaps(Range other) {
+            Objects.requireNonNull(other);
+
+            return ((this.start <= other.start) && (this.end >= other.end)) ||
+                ((other.start <= this.start) && (other.end >= this.end));
+        } //overlaps
+    } //Range
+
+    private static int parseInt(Matcher matcher, int groupIndex) {
+        Objects.requireNonNull(matcher);
+
+        int groupCount = matcher.groupCount();
+
+        if (groupIndex > groupCount) {
+            throw new IllegalArgumentException();
+        } //end if
+
+        String groupString = matcher.group(groupIndex);
+
+        return Integer.parseInt(groupString);
+    } //parseInt
+
+    public static void main(String[] args) {
+        Path path = Path.of("src/main/resources/day4/input.txt");
+
+        List<String> lines;
+
+        try {
+            lines = Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return;
+        } //end try catch
+
+        String regex = "^(\\d+)-(\\d+),(\\d+)-(\\d+)$";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        int overlapCount = 0;
+
+        for (String line : lines) {
+            Matcher matcher = pattern.matcher(line);
+
+            if (!matcher.matches()) {
+                throw new IllegalStateException();
+            } //end if
+
+            int start0 = CampCleanupPart1.parseInt(matcher, 1);
+
+            int end0 = CampCleanupPart1.parseInt(matcher, 2);
+
+            Range range0 = new Range(start0, end0);
+
+            int start1 = CampCleanupPart1.parseInt(matcher, 3);
+
+            int end1 = CampCleanupPart1.parseInt(matcher, 4);
+
+            Range range1 = new Range(start1, end1);
+
+            if (range0.overlaps(range1)) {
+                overlapCount++;
+            } //end if
+        } //end for
+
+        System.out.printf("Overlap Count: %d%n", overlapCount);
+    } //main
+}
