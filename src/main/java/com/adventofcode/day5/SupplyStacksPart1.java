af60c9a69e1a93a40060c056a3fbb18ecdf05535
Logan <rashes_lineage02@icloud.com>
2022-12-10 14:01:38 -0600

    Day 5 Part 1

--- src/main/java/com/adventofcode/day5/SupplyStacksPart1.java
+++ src/main/java/com/adventofcode/day5/SupplyStacksPart1.java
@@ -47,8 +47,130 @@ public final class SupplyStacksPart1 {
         return Integer.parseInt(stackCountString);
     } //getStackCount
 
+    private record Result(List<Deque<String>> stacks, List<String> instructions) {
+    } //Result
+
+    private static Result getStacksResult(List<String> lines) {
+        Objects.requireNonNull(lines);
+
+        int stackCount = SupplyStacksPart1.getStackCount(lines);
+
+        List<Deque<String>> stacks = new ArrayList<>(stackCount);
+
+        for (int i = 0; i < stackCount; i++) {
+            Deque<String> deque = new ArrayDeque<>();
+
+            stacks.add(deque);
+        } //end for
+
+        String regex = ".(.).\\s?";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Integer instructionsIndex = null;
+
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+
+            String indicesRegex = "\\s(\\d\\s+)+";
+
+            if (line.matches(indicesRegex)) {
+                instructionsIndex = i + 2;
+
+                break;
+            } //end if
+
+            Matcher matcher = pattern.matcher(line);
+
+            List<String> results = matcher.results()
+                                          .map(matchResult -> matchResult.group(1))
+                                          .toList();
+
+            for (int j = 0; j < results.size(); j++) {
+                String crate = results.get(j);
+
+                if (crate.isBlank()) {
+                    continue;
+                } //end if
+
+                stacks.get(j)
+                      .addLast(crate);
+            } //end for
+        } //end for
+
+        if (instructionsIndex == null) {
+            throw new IllegalStateException();
+        } //end if
+
+        int endIndex = lines.size();
+
+        List<String> instructions = lines.subList(instructionsIndex, endIndex);
+
+        return new Result(stacks, instructions);
+    } //getStacksResult
+
+    private static int getGroup(Matcher matcher, int group) {
+        String groupString = matcher.group(group);
+
+        return Integer.parseInt(groupString);
+    } //getGroup
+
+    private static List<Deque<String>> getModifiedStacks(List<String> lines) {
+        Objects.requireNonNull(lines);
+
+        Result stacksResult = SupplyStacksPart1.getStacksResult(lines);
+
+        List<String> instructions = stacksResult.instructions();
+
+        String regex = "^move (\\d+) from (\\d+) to (\\d+)$";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        List<Deque<String>> stacks = stacksResult.stacks();
+
+        for (String instruction : instructions) {
+            Matcher matcher = pattern.matcher(instruction);
+
+            if (!matcher.matches()) {
+                throw new IllegalStateException();
+            } //end if
+
+            int crateCount = SupplyStacksPart1.getGroup(matcher, 1);
+
+            int sourceStack = SupplyStacksPart1.getGroup(matcher, 2) - 1;
+
+            int targetStack = SupplyStacksPart1.getGroup(matcher, 3) - 1;
+
+            for (int i = 0; i < crateCount; i++) {
+                String crate = stacks.get(sourceStack)
+                                     .removeFirst();
+
+                stacks.get(targetStack)
+                      .addFirst(crate);
+            } //end for
+        } //end for
+
+        return stacks;
+    } //getModifiedStacks
+
+    private static String getModifiedTopCrates(List<String> lines) {
+        Objects.requireNonNull(lines);
+
+        List<Deque<String>> stacks = SupplyStacksPart1.getModifiedStacks(lines);
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Deque<String> stack : stacks) {
+            String topCrate = stack.peekFirst();
+
+            stringBuilder.append(topCrate);
+        } //end for
+
+        return stringBuilder.toString();
+    } //getModifiedTopCrates
+
     public static void main(String[] args) {
-        Path path = Path.of("src/main/resources/day5/sample.txt");
+        Path path = Path.of("src/main/resources/day5/input.txt");
 
         List<String> lines;
 
@@ -64,48 +186,8 @@ public final class SupplyStacksPart1 {
             throw new IllegalStateException();
         } //end while
 
-        int stackCount = SupplyStacksPart1.getStackCount(lines);
+        String topCrates = SupplyStacksPart1.getModifiedTopCrates(lines);
 
-        @SuppressWarnings("unchecked")
-        Deque<String>[] stacks = (Deque<String>[]) new Deque[stackCount];
-
-        for (int i = 0; i < stackCount; i++) {
-            stacks[i] = new ArrayDeque<>();
-        } //end for
-
-        System.out.println(stackCount);
-
-        String regex = ".(.).\\s?";
-
-        System.out.println(regex);
-
-        Pattern pattern = Pattern.compile(regex);
-
-        for (String line : lines) {
-            System.out.println(line);
-
-            if (line.matches("\\s(\\d\\s+)+")) {
-                break;
-            } //end if
-
-            Matcher matcher = pattern.matcher(line);
-
-            List<String> results = matcher.results()
-                                          .map(matchResult -> matchResult.group(1))
-                                          .toList();
-
-            for (int j = 0; j < results.size(); j++) {
-                String item = results.get(j);
-
-                if (item.isBlank()) {
-                    continue;
-                } //end if
-
-                stacks[j].addLast(item);
-            } //end for
-        } //end for
-
-        Arrays.stream(stacks)
-              .forEach(System.out::println);
+        System.out.printf("Top Crates: %s%n", topCrates);
     } //main
 }
